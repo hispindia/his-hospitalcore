99f35f0f735c0c5df5fc885b59350811f78bc054
Shubham Goyal <goylshubham@gmail.com>
2020-10-08 14:15:00 +0530

    added validation to query parameters

--- omod/src/main/java/org/openmrs/module/hospitalcore/web/controller/apiController/PatientDetailsController.java
+++ omod/src/main/java/org/openmrs/module/hospitalcore/web/controller/apiController/PatientDetailsController.java
@@ -1,6 +1,5 @@
 package org.openmrs.module.hospitalcore.web.controller.apiController;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -26,53 +25,8 @@ public class PatientDetailsController {
 
     @RequestMapping(method = RequestMethod.GET)
     public void getApi(HttpServletResponse response, HttpServletRequest request,
-            @RequestParam(value = "pi", required = true) String pi,
-            @RequestParam(value = "date", required = true) String date) throws Exception {
-
-        boolean notfound = false;
-
-        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
-
-        String query = "select ps.patient_id, ps.identifier, concat_ws(' ', ps.given_name, ps.middle_name, ps.family_name) as patientName, ps.gender, concat('', encounter.encounter_datetime) as visit_date, cn.name as opd_visit, case when TIMESTAMPDIFF(year, ps.birthdate, encounter.encounter_datetime) < 1 then case when TIMESTAMPDIFF(month, ps.birthdate, encounter.encounter_datetime) < 1 then concat_ws(' ', TIMESTAMPDIFF(day, ps.birthdate, encounter.encounter_datetime), 'd') else concat_ws(' ', TIMESTAMPDIFF(month, ps.birthdate, encounter.encounter_datetime), 'm') end else concat_ws(' ', TIMESTAMPDIFF(year, ps.birthdate, encounter.encounter_datetime), 'y') end as age from patient_search ps inner join encounter on encounter.patient_id = ps.patient_id inner join obs on obs.encounter_id = encounter.encounter_id inner join concept_name cn on cn.concept_id = obs.value_coded and cn.concept_name_type = 'FULLY_SPECIFIED' where ps.identifier  = '"
-                + pi + "' AND date(encounter.encounter_datetime) = '" + date + "' AND obs.concept_id = 3";
-
-        List<Map<String, Object>> patientDetails = jdbcTemplate.queryForList(query);
-
-        query = "select case when gp.property = 'hospital.location_user' then 'hospitalName' else 'ninCode' end as property, gp.property_value from global_property gp where gp.property in ('hospital.location_user', 'hospitalcore.ninCode')";
-
-        List<Map<String, Object>> hospitalProperties = jdbcTemplate.queryForList(query);
-
-        Map<String, Object> notFoundError = new HashMap<String, Object>();
-
-        PatientDetail patientDetail = new PatientDetail();
-
-        if (patientDetails.size() == 0) {
-            notfound = true;
-            notFoundError.put("Error", "No record found.");
-        } else {
-            patientDetail.setPatientName((String) patientDetails.get(0).get("patientName"));
-            patientDetail.setAge((String) patientDetails.get(0).get("age"));
-            patientDetail.setGender((String) patientDetails.get(0).get("gender"));
-            patientDetail.setVisitDateTime(patientDetails.get(0).get("visit_date").toString());
-            patientDetail.setVisitOpd((String) patientDetails.get(0).get("opd_visit"));
-            patientDetail.setPatientIdentifier((String) patientDetails.get(0).get("identifier"));
-            patientDetail.setHospitalName((String) hospitalProperties.get(0).get("property_value"));
-            patientDetail.setNinCode((String) hospitalProperties.get(1).get("property_value"));
-
-            query = "select cn.name, cm.source_code as code, concat_ws(' ', pn.given_name, pn.middle_name, pn.family_name) as doctorName from obs inner join concept_name cn on cn.concept_id = obs.value_coded and cn.concept_name_type = 'FULLY_SPECIFIED' inner join concept_name cn1 on cn1.concept_id = obs.concept_id and cn1.concept_name_type = 'FULLY_SPECIFIED' inner join concept_map cm on cm.concept_id = cn.concept_id inner join concept_source cs on cs.concept_source_id = cm.source and cs.name = 'ICD-10-WHO' inner join users on users.user_id = obs.creator inner join person on person.person_id = users.person_id inner join person_name pn on pn.person_id = person.person_id where obs.person_id = "
-                    + (Integer) patientDetails.get(0).get("patient_id") + " and date(obs_datetime) = '" + date
-                    + "' AND cn1.name in ('FINAL DIAGNOSIS', 'PROVISIONAL DIAGNOSIS') group by obs.value_coded";
-
-            List<Map<String, Object>> diagnosis = jdbcTemplate.queryForList(query);
-
-            String doctorName = "";
-            for (Map<String, Object> map : diagnosis) {
-                doctorName = (String) map.get("doctorName");
-                map.remove("doctorName");
-            }
-            patientDetail.setDoctorName(doctorName);
-            patientDetail.setDiagnosis(diagnosis);
-        }
+            @RequestParam(value = "pi", required = false) String pi,
+            @RequestParam(value = "date", required = false) String date) throws Exception {
 
         String requestOrigin = request.getHeader("Origin");
 
@@ -81,17 +35,69 @@ public class PatientDetailsController {
 
         ServletOutputStream out = response.getOutputStream();
 
-        if (notfound) {
-            new ObjectMapper().writeValue(out, notFoundError);
+        Error error = new Error();
+
+        if (request.getParameter("pi") == null || request.getParameter("date") == null) {
+            error.setError("query parameters are missing");
+            new ObjectMapper().writeValue(out, error);
         } else {
-            new ObjectMapper().writeValue(out, patientDetail);
+            JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+
+            String query = "select ps.patient_id, ps.identifier, concat_ws(' ', ps.given_name, ps.middle_name, ps.family_name) as patientName, ps.gender, concat('', encounter.encounter_datetime) as visit_date, cn.name as opd_visit, case when TIMESTAMPDIFF(year, ps.birthdate, encounter.encounter_datetime) < 1 then case when TIMESTAMPDIFF(month, ps.birthdate, encounter.encounter_datetime) < 1 then concat_ws(' ', TIMESTAMPDIFF(day, ps.birthdate, encounter.encounter_datetime), 'd') else concat_ws(' ', TIMESTAMPDIFF(month, ps.birthdate, encounter.encounter_datetime), 'm') end else concat_ws(' ', TIMESTAMPDIFF(year, ps.birthdate, encounter.encounter_datetime), 'y') end as age from patient_search ps inner join encounter on encounter.patient_id = ps.patient_id inner join obs on obs.encounter_id = encounter.encounter_id inner join concept_name cn on cn.concept_id = obs.value_coded and cn.concept_name_type = 'FULLY_SPECIFIED' where ps.identifier  = '"
+                    + pi + "' AND date(encounter.encounter_datetime) = '" + date + "' AND obs.concept_id = 3";
+
+            List<Map<String, Object>> patientDetails = jdbcTemplate.queryForList(query);
+
+            if (patientDetails.size() == 0) {
+                error.setError("No record found");
+                new ObjectMapper().writeValue(out, error);
+            } else {
+                query = "select case when gp.property = 'hospital.location_user' then 'hospitalName' else 'ninCode' end as property, gp.property_value from global_property gp where gp.property in ('hospital.location_user', 'hospitalcore.ninCode')";
+
+                List<Map<String, Object>> hospitalProperties = jdbcTemplate.queryForList(query);
+
+                PatientDetail patientDetail = new PatientDetail();
+
+                patientDetail.setPatientName((String) patientDetails.get(0).get("patientName"));
+                patientDetail.setAge((String) patientDetails.get(0).get("age"));
+                patientDetail.setGender((String) patientDetails.get(0).get("gender"));
+                patientDetail.setVisitDateTime(patientDetails.get(0).get("visit_date").toString());
+                patientDetail.setVisitOpd((String) patientDetails.get(0).get("opd_visit"));
+                patientDetail.setPatientIdentifier((String) patientDetails.get(0).get("identifier"));
+                patientDetail.setHospitalName((String) hospitalProperties.get(0).get("property_value"));
+                patientDetail.setNinCode((String) hospitalProperties.get(1).get("property_value"));
+
+                query = "select cn.name, cm.source_code as code, concat_ws(' ', pn.given_name, pn.middle_name, pn.family_name) as doctorName from obs inner join concept_name cn on cn.concept_id = obs.value_coded and cn.concept_name_type = 'FULLY_SPECIFIED' inner join concept_name cn1 on cn1.concept_id = obs.concept_id and cn1.concept_name_type = 'FULLY_SPECIFIED' inner join concept_map cm on cm.concept_id = cn.concept_id inner join concept_source cs on cs.concept_source_id = cm.source and cs.name = 'ICD-10-WHO' inner join users on users.user_id = obs.creator inner join person on person.person_id = users.person_id inner join person_name pn on pn.person_id = person.person_id where obs.person_id = "
+                        + (Integer) patientDetails.get(0).get("patient_id") + " and date(obs_datetime) = '" + date
+                        + "' AND cn1.name in ('FINAL DIAGNOSIS', 'PROVISIONAL DIAGNOSIS') group by obs.value_coded";
+
+                List<Map<String, Object>> diagnosis = jdbcTemplate.queryForList(query);
+
+                if (diagnosis.size() > 0) {
+                    patientDetail.setDoctorName((String) diagnosis.get(0).get("doctorName"));
+                    for (Map<String, Object> map : diagnosis) {
+                        map.remove("doctorName");
+                    }
+                }
+                patientDetail.setDiagnosis(diagnosis);
+                new ObjectMapper().writeValue(out, patientDetail);
+            }
         }
     }
 }
 
-/**
- * InnerPatientDetailsController
- */
+class Error {
+    String error;
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+}
+
 class PatientDetail {
     String hospitalName;
     String ninCode;
@@ -149,7 +155,7 @@ class PatientDetail {
     }
 
     public void setPatientName(String patientName) {
-        this.patientName = patientName;
+        this.patientName = patientName.replaceAll("\\s+", " ").trim();
     }
 
     public void setDiagnosis(List<Map<String, Object>> diagnosis) {
@@ -169,11 +175,11 @@ class PatientDetail {
     }
 
     public void setVisitOpd(String visitOpd) {
-        this.visitOpd = visitOpd;
+        this.visitOpd = visitOpd.replaceAll("\\s+", " ").trim();
     }
 
     public void setHospitalName(String hospitalName) {
-        this.hospitalName = hospitalName;
+        this.hospitalName = hospitalName.replaceAll("\\s+", " ").trim();
     }
 
     public void setNinCode(String ninCode) {
@@ -181,6 +187,6 @@ class PatientDetail {
     }
 
     public void setDoctorName(String doctorName) {
-        this.doctorName = doctorName;
+        this.doctorName = doctorName.replaceAll("\\s+", " ").trim();
     }
 }
